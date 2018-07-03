f40b74c145af97e3ea34c1f42c0421b3fd23763c
Brad Chao <brad@brad.tw>
2018-07-03 11:10:13 +0800

    v1: 版面配置 step 1

---
+++ app/src/main/java/tw/brad/ry_lottery/MainActivity.java
@@ -0,0 +1,13 @@
+package tw.brad.ry_lottery;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
